b99c3f5e4f40f74c37f6b4e6fb6d7f23a612b82e
Abraham <gonzalezriveroabraham@gmail.com>
2022-12-26 16:06:16 -0500

    map changes

--- Cell.py
+++ Cell.py
@@ -1,10 +1,34 @@
 
 class Cell:
-    def __init__(self,x,y):
-        self.X=x
-        self.Y=y
-    
-class CellOfWater(Cell):
-    def __init__(self, x, y):
+    def __init__(self):
+        self.food = 0
+        self.animal = None
+        self.restrictions = [0,0,0]
+class WaterCell(Cell):
+    def __init__(self):
         self.typeOfCell = "water"
-        super().__init__(x, y)
+        super().__init__(self)
+        self.restrictions = [0,-2,-3]
+        
+class ForestCell(Cell):
+    def __init__(self):
+        self.typeOfCell = "forest"
+        super().__init__(self)
+        self.restrictions = [-2,0,0]
+class MountainCell(Cell):
+    def __init__(self):
+        self.typeOfCell = "mountain"
+        super().__init__(self)
+        self.restrictions = [0,-2,-2]
+class MeadowCell(Cell):
+    def __init__(self):
+        self.typeOfCell = "meadow"
+        super().__init__(self)
+        self.restrictions = [-1,0,0]
+        self.food += 10
+class PlainCell(Cell):
+    def __init__(self):
+        self.typeOfCell = "plain"
+        super().__init__(self)
+        
+                
--- Map.py
+++ Map.py
@@ -1,7 +1,7 @@
 from Cell import Cell
 
 class Map:
-    def __init__(self, col_amount:int,row_amount=int):
+    def __init__(self, col_amount:int,row_amount:int):
         self.col = col_amount
         self.row = row_amount
         self.cell_List=[[Cell]]
@@ -15,4 +15,6 @@ class Map:
         # TODO: Implemnt an IA for create the feature of every cell in the map
         pass
     
-
+class TestMap(Map):
+    def __init__(self, testMap:list[list[Cell]]):
+        self.cell_List = testMap 
