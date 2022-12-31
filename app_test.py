6bb8f62381f60fddca9e98f5c88b167f2f9a67fd
Abraham <gonzalezriveroabraham@gmail.com>
2022-12-31 05:52:09 -0500

    Map Gen Update

--- app_test.py
+++ app_test.py
@@ -18,9 +18,9 @@ import numpy as np
 # b[0,0] = 1
 # print(np.argmax(a))
 
-map = Map(5,5)
+map = Map(17,17)
 map.Builder_Map()
-vara = RandomCellForGenerate(map,5)
+vara = RandomCellForGenerate(map,41)
 variables = ["lista1", "lista2", "lista3", "lista4", "lista5"]
 domain : Dict[str,list[str]] = {}
 
@@ -80,21 +80,50 @@ print("Primer Mapa")
 print(" ")
 
 myMap = MapGenerate(map.cell_List)
-myMap = myMap.generate()
+map = None
+for i in range(3):
+    map = myMap.generate()
+    myMap = MapGenerate(map)
 
-listOfCell = [] 
-i=0
-for lista in myMap:
-    listOfCell.append([])
-    for cell in lista:
-        listOfCell[i].append(cell.icon)
-    i+=1
+    listOfCell = [] 
+    i=0
+    for lista in map:
+        listOfCell.append([])
+        for cell in lista:
+            listOfCell[i].append(cell.icon)
+        i+=1
 
-for i in range(len(listOfCell)):
-    print(listOfCell[i])
+    for i in range(len(listOfCell)):
+        for j in range(len(listOfCell[i])):
+            if j==len(listOfCell[i])-1:
+                if listOfCell[i][j] == "P":
+                    print(Fore.YELLOW,listOfCell[i][j])
+                elif listOfCell[i][j] == "W":
+                    print(Fore.BLUE,listOfCell[i][j])
+                elif listOfCell[i][j] == "M":
+                    print(Fore.MAGENTA,listOfCell[i][j])
+                elif listOfCell[i][j] == "F":
+                    print(Fore.RED,listOfCell[i][j])
+                elif listOfCell[i][j] == "H":
+                    print(Fore.GREEN,listOfCell[i][j])
+                else:
+                    print(Fore.WHITE,""+listOfCell[i][j])
+            else:
+                if listOfCell[i][j] == "P":
+                    print(Fore.YELLOW, listOfCell[i][j], end="")
+                elif listOfCell[i][j] == "W":
+                    print(Fore.BLUE, listOfCell[i][j], end="")
+                elif listOfCell[i][j] == "M":
+                    print(Fore.MAGENTA, listOfCell[i][j], end="")
+                elif listOfCell[i][j] == "F":
+                    print(Fore.RED, listOfCell[i][j], end="")
+                elif listOfCell[i][j] == "H":
+                    print(Fore.GREEN, listOfCell[i][j], end="")
+                else:
+                    print(Fore.WHITE,""+listOfCell[i][j], end="")
 
-print("Segundo Mapa")
-print(" ")
+    print("Primer Mapa")
+    print(" ")
 # Make a boolean mask for vara to create the kind of cells that the csp give us 
 
 
