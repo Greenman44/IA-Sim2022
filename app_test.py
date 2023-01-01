1a45fa1efaac50aee48b5691582544d697731a8e
Ernesto Alfonso <ernealfonso00@gmail.com>
2023-01-01 18:25:45 -0500

    To prove map with simulation

--- app_test.py
+++ app_test.py
@@ -20,11 +20,12 @@ import numpy as np
 
 map = Map(50,50)
 map.Builder_Map()
-vara = RandomCellForGenerate(map,700)
+vara = RandomCellForGenerate(map,400)
 variables = ["lista1", "lista2", "lista3", "lista4", "lista5"]
 domain : Dict[str,list[str]] = {}
+monkey = Monkey(0,0)
 
-
+map[0,0].animals.append(monkey)
 
 for var in variables:
     domain[var] = {"water", "forest", "mountain", "meadow", "plain"}
@@ -44,86 +45,86 @@ i=0
 for lista in map.cell_List:
     listOfCell.append([])
     for cell in lista:
-        listOfCell[i].append(cell.icon)
+        listOfCell[i].append(cell)
     i+=1
 
 for i in range(len(listOfCell)):
     for j in range(len(listOfCell[i])):
         if j==len(listOfCell[i])-1:
-            if listOfCell[i][j] == "P":
-                print(Fore.YELLOW,listOfCell[i][j])
-            elif listOfCell[i][j] == "W":
-                print(Fore.BLUE,listOfCell[i][j])
-            elif listOfCell[i][j] == "M":
-                print(Fore.MAGENTA,listOfCell[i][j])
-            elif listOfCell[i][j] == "F":
-                print(Fore.RED,listOfCell[i][j])
-            elif listOfCell[i][j] == "H":
-                print(Fore.GREEN,listOfCell[i][j])
-            else:
-                print(Fore.WHITE,""+listOfCell[i][j])
+            if listOfCell[i][j].animals != None:
+                print(listOfCell[i][j].animals[0].icon)
+            if listOfCell[i][j].icon == "P":
+                print(Fore.YELLOW,listOfCell[i][j].icon)
+            elif listOfCell[i][j].icon == "W":
+                print(Fore.BLUE,listOfCell[i][j].icon)
+            elif listOfCell[i][j].icon == "M":
+                print(Fore.MAGENTA,listOfCell[i][j].icon)
+            elif listOfCell[i][j].icon == "F":
+                print(Fore.RED,listOfCell[i][j].icon)
+            elif listOfCell[i][j].icon == "H":
+                print(Fore.GREEN,listOfCell[i][j].icon)
         else:
-            if listOfCell[i][j] == "P":
-                print(Fore.YELLOW, listOfCell[i][j], end="")
-            elif listOfCell[i][j] == "W":
-                print(Fore.BLUE, listOfCell[i][j], end="")
-            elif listOfCell[i][j] == "M":
-                print(Fore.MAGENTA, listOfCell[i][j], end="")
-            elif listOfCell[i][j] == "F":
-                print(Fore.RED, listOfCell[i][j], end="")
-            elif listOfCell[i][j] == "H":
-                print(Fore.GREEN, listOfCell[i][j], end="")
-            else:
-                print(Fore.WHITE,""+listOfCell[i][j], end="")
+            if listOfCell[i][j].animals != None:
+                print(listOfCell[i][j].animals[0].icon, end="")
+            if listOfCell[i][j].icon == "P":
+                print(Fore.YELLOW, listOfCell[i][j].icon, end="")
+            elif listOfCell[i][j].icon == "W":
+                print(Fore.BLUE, listOfCell[i][j].icon, end="")
+            elif listOfCell[i][j].icon == "M":
+                print(Fore.MAGENTA, listOfCell[i][j].icon, end="")
+            elif listOfCell[i][j].icon == "F":
+                print(Fore.RED, listOfCell[i][j].icon, end="")
+            elif listOfCell[i][j].icon == "H":
+                print(Fore.GREEN, listOfCell[i][j].icon, end="")
 
 print("Primer Mapa")
 print(" ")
 
-myMap = MapGenerate(map.cell_List)
-map = None
-for i in range(1):
-    map = myMap.generate()
-    myMap = MapGenerate(map)
+# myMap = MapGenerate(map.cell_List)
+# map = None
+# for i in range(1):
+#     map = myMap.generate()
+#     myMap = MapGenerate(map)
 
-    listOfCell = [] 
-    i=0
-    for lista in map:
-        listOfCell.append([])
-        for cell in lista:
-            listOfCell[i].append(cell.icon)
-        i+=1
+#     listOfCell = [] 
+#     i=0
+#     for lista in map:
+#         listOfCell.append([])
+#         for cell in lista:
+#             listOfCell[i].append(cell.icon)
+#         i+=1
 
-    for i in range(len(listOfCell)):
-        for j in range(len(listOfCell[i])):
-            if j==len(listOfCell[i])-1:
-                if listOfCell[i][j] == "P":
-                    print(Fore.YELLOW,listOfCell[i][j])
-                elif listOfCell[i][j] == "W":
-                    print(Fore.BLUE,listOfCell[i][j])
-                elif listOfCell[i][j] == "M":
-                    print(Fore.MAGENTA,listOfCell[i][j])
-                elif listOfCell[i][j] == "F":
-                    print(Fore.RED,listOfCell[i][j])
-                elif listOfCell[i][j] == "H":
-                    print(Fore.GREEN,listOfCell[i][j])
-                else:
-                    print(Fore.WHITE,""+listOfCell[i][j])
-            else:
-                if listOfCell[i][j] == "P":
-                    print(Fore.YELLOW, listOfCell[i][j], end="")
-                elif listOfCell[i][j] == "W":
-                    print(Fore.BLUE, listOfCell[i][j], end="")
-                elif listOfCell[i][j] == "M":
-                    print(Fore.MAGENTA, listOfCell[i][j], end="")
-                elif listOfCell[i][j] == "F":
-                    print(Fore.RED, listOfCell[i][j], end="")
-                elif listOfCell[i][j] == "H":
-                    print(Fore.GREEN, listOfCell[i][j], end="")
-                else:
-                    print(Fore.WHITE,""+listOfCell[i][j], end="")
+#     for i in range(len(listOfCell)):
+#         for j in range(len(listOfCell[i])):
+#             if j==len(listOfCell[i])-1:
+#                 if listOfCell[i][j] == "P":
+#                     print(Fore.YELLOW,listOfCell[i][j])
+#                 elif listOfCell[i][j] == "W":
+#                     print(Fore.BLUE,listOfCell[i][j])
+#                 elif listOfCell[i][j] == "M":
+#                     print(Fore.MAGENTA,listOfCell[i][j])
+#                 elif listOfCell[i][j] == "F":
+#                     print(Fore.RED,listOfCell[i][j])
+#                 elif listOfCell[i][j] == "H":
+#                     print(Fore.GREEN,listOfCell[i][j])
+#                 else:
+#                     print(Fore.WHITE,""+listOfCell[i][j])
+#             else:
+#                 if listOfCell[i][j] == "P":
+#                     print(Fore.YELLOW, listOfCell[i][j], end="")
+#                 elif listOfCell[i][j] == "W":
+#                     print(Fore.BLUE, listOfCell[i][j], end="")
+#                 elif listOfCell[i][j] == "M":
+#                     print(Fore.MAGENTA, listOfCell[i][j], end="")
+#                 elif listOfCell[i][j] == "F":
+#                     print(Fore.RED, listOfCell[i][j], end="")
+#                 elif listOfCell[i][j] == "H":
+#                     print(Fore.GREEN, listOfCell[i][j], end="")
+#                 else:
+#                     print(Fore.WHITE,""+listOfCell[i][j], end="")
 
-    print("Primer Mapa")
-    print(" ")
+#     print("Primer Mapa")
+#     print(" ")
 # Make a boolean mask for vara to create the kind of cells that the csp give us 
 
 
