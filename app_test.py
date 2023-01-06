bf328534303e6cf962f7fb6575453bd455992259
jorgesolerrr <jorgeasoler2015@gmail.com>
2023-01-06 14:03:14 -0500

    final changes

--- app_test.py
+++ app_test.py
@@ -1,6 +1,14 @@
 from Generators.Map_Generator import gen_map
 from Simulation.Prey_vs_Predator import *
 from iAalgorithm.SimulatedAnnealing import SimulatedAnnealingEcoSystem
+from os import getcwd, mkdir
+
+#create folder to store results
+try:
+    mkdir(getcwd() + "/Result_Of_Plays")
+except:
+    pass 
+
 
 #change parameters to generate other map
 map = gen_map(15,15, preys_amount= 10,preds_amount=4,max_pred_food=30, max_prey_food=30)
